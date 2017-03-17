7b335977879e56fb8bc211ac13e920b9ec425c94
Bobby Speirs <bobby.speirs@gmail.com>
2017-03-17 13:21:55 -0400

    Added input field

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { Message } from "./messages/message.model";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    messages: Message[] = [
-      new Message('Some message', 'Bobby'),
-      new Message('Something else', 'Bobby'),
-      new Message('Another message', 'Somebody else')
-    ];
+
 }
