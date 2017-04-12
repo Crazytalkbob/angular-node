d888ae1ea65ae4a492f4358f9639a79203c617fa
Bobby Speirs <bobby.speirs@gmail.com>
2017-04-12 16:49:39 -0400

    Production build

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: `
+  template: `
     <header class="row">
       <nav class="col-md-8 col-md-offset-2">
         <ul class="nav nav-pills">
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,7 +1,7 @@
 import { Http, Response, Headers } from "@angular/http";
 import { Injectable, EventEmitter } from "@angular/core";
 import 'rxjs/Rx';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from "rxjs";
 
 import { Message } from "./message.model";
 import { ErrorService } from "../errors/error.service";
